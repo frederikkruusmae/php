c6581d86200b473191b93c783cfc83ea56e280db
frederikkruusmae <frederikkru@gmail.com>
2024-02-01 13:38:42 +0200

    xd

--- h3.php
+++ h3.php
@@ -9,13 +9,37 @@
   <body>
     <div class="container">
     <h1>Ülesanne3</h1>
-    <?php
-    /*
-    Ülesanne 1
-    Frederik Kruusimäe
-    01.02.2024
-    */
     
+    <form action="#" method="get">
+        <label for="a">külg a</label><br>
+        <input type="number" name="a" id="a"><br>
+        <label for="b">külg b</label><br>
+        <input type="number" name="b" id="b"><br>
+        <label for="h">kõrgus h</label><br>
+        <input type="number" name="h" id="h"><br>
+        <input type="submit" value="Arvuta">
+    </form>
+    <?php
+    if(!empty($_GET)){
+        $a = $_GET["a"];
+        $b = $_GET["b"];
+        $h = $_GET["h"];
+        $s = (($a+$b)/2)*$h;
+        echo "Trapetsi pindala on ".$s."<br>";
+    }
+    ?>
+    <hr>
+    <h2>rombi ümbermõõt</h2>
+    <form action="#" method="get">
+        <label for="a">Rombi külg</label><br>
+        <input type="number" name="a" id="a"><br>
+        <input type="submit" value="Arvuta">
+    <?php
+    if(!empty($_GET)){
+        $aa = $_GET["a"];
+        $c = $a*4;
+        echo "Rombi ümbermõõt on ".$c."<br>";
+    }
     ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
