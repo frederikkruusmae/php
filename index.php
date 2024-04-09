4a38de39712e0d61bcd0cd1953377bb05aa8d15b
frederikkruusmae <115454921+frederikkruusmae@users.noreply.github.com>
2024-04-09 08:55:43 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kõik huinja</title>
+</head>
+<body>
+    <h1>Tere MaRiO!</h1>
+    <ul>
+        <li><a href="01.php">Harjutus1</a></li>
+        <li><a href="h2.php">Harjutus2</a></li>
+        <li><a href="h3.php">Harjutus3</a></li>
+        <li><a href="h4.php">Harjutus4</a></li>
+        <li><a href="h5.php">Harjutus5</a></li>
+        <li><a href="h6.php">Harjutus6</a></li>
+        <li><a href="h7.php">Harjutus7</a></li>
+        <li><a href="h8.php">Harjutus8</a></li>
+        <li><a href="h9.php">Harjutus9</a></li>
+        <li><a href="h10.php">Harjutus10</a></li>
+        <li><a href="h12.php">Harjutus12</a></li>
+        <li><a href="h13.php">Harjutus13</a></li>
+        <li><a href="h14.php">Harjutus14</a></li>
+        <li><a href="Kontrolltoo_kontakt.php">Kontrolltoo_kontakt</a></li>
+        <li><a href="kontrolltoo_tooted.php">Kontrolltoo_tooted</a></li>
+        <li><a href="kontrolltoo.php">Kontrolltoo</a></li>
+        <li><a href="leht1.php">leht1</a></li>
+        <li><a href="leht2.php">leht2</a></li>
+        <li><a href="leht3.php">leht3</a></li>
+        
+    </ul>
+</body>
+</html>
